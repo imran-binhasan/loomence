be0985ab22634f57bc8a647cfb86115aba80cdb3
imran-binhasan <imranbinhasan.work@gmail.com>
2025-04-19 19:58:54 +0600

    feat(header): add responsiveness for all screen

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,9 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
+
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { APP_NAME } from "@/lib/constsants"; // typo?
-import ThemeToggle from "./theme-toggle";
+import { APP_NAME } from "@/lib/constsants"; 
+import Menu from "./menu";
 
 const Header = () => {
   return (
@@ -20,23 +20,7 @@ const Header = () => {
           />
           <span className="hidden lg:block font-poppins font-bold text-2xl">{APP_NAME}</span>
         </Link>
-
-        {/* Actions: Cart & Sign In */}
-        <div className="flex items-center space-x-2">
-          <ThemeToggle/>
-          <Button asChild variant="ghost">
-            <Link href="/cart" className="flex items-center gap-1">
-              <ShoppingCart className="w-5 h-5" />
-              <span>Cart</span>
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in" className="flex items-center gap-1">
-              <UserIcon className="w-5 h-5" />
-              <span>Sign In</span>
-            </Link>
-          </Button>
-        </div>
+         <Menu/>
       </div>
     </header>
   );
---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,60 @@
+import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import ThemeToggle from "./theme-toggle";
+import Link from "next/link";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const Menu = () => {
+  return (
+    <div className="flex justify-center gap-3">
+      <nav className="hidden md:flex w-full max-w-xs gap-1 ">
+        <ThemeToggle />
+        <Button asChild variant="ghost">
+          <Link href="/cart" className="flex items-center gap-1">
+            <ShoppingCart className="w-5 h-5" />
+            <span>Cart</span>
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href="/sign-in" className="flex items-center gap-1">
+            <UserIcon className="w-5 h-5" />
+            <span>Sign In</span>
+          </Link>
+        </Button>
+      </nav>
+      <nav className="md:hidden">
+        <Sheet>
+          <SheetTrigger className="align-middle">
+            <EllipsisVertical />
+          </SheetTrigger>
+          <SheetContent className="border flex flex-col items-start px-1.5 py-3">
+            <SheetTitle className="px-3 py1.5">Menu</SheetTitle>
+            
+            <Button asChild variant="ghost">
+              <Link href="/cart" className="flex items-center gap-1">
+                <ShoppingCart className="w-5 h-5" />
+                <span>Cart</span>
+              </Link>
+            </Button>
+            <Button asChild variant={'ghost'}>
+              <Link href="/sign-in" className="flex items-center gap-1">
+                <UserIcon className="w-5 h-5" />
+                <span>Sign In</span>
+              </Link>
+            </Button>
+
+            <SheetDescription>..</SheetDescription>
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </div>
+  );
+};
+
+export default Menu;
